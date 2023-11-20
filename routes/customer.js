61bec4ab0c2394338865782b1e5609bfd4c101fc
yourstejasvi <tejasvis3103@gmail.com>
2023-11-20 12:47:10 +0530

    Second commit

--- routes/customer.js
+++ routes/customer.js
@@ -1,22 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
-// var pool= require('pool');
+var pool= require('./pool');
 var multer = require('multer');
+router.use(bodyParser.json());
 
-// router.post("/register_customer",function(req,res){
-//     pool.query("INSERT INTO registration_customer (fname, lname, email, mob, city, password) VALUES(?,?,?,?,?,?)"), [req.body], function(error,result){
-//         if(error){
-//             console.error("Error inserting record", error);
-//             res.status(500).json({error:"Failed to register customer"});
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
-//         }
-//         else{
-//             console.log("Registration successful");
-//             res.json({message: "Customer registrated successfully"});
-//         }
-//     }
-        
-// });
+router.post("/register_customer", function (req, res) {
+    console.log(req.body);
+    pool.query(
+        "INSERT INTO registration_customer (fname, lname, mail, mob, city, password) VALUES(?,?,?,?,?,?)",
+        [req.body.fname, req.body.lname, req.body.mail, req.body.mob, req.body.city, req.body.password],
+        function (error, result) {
+            if (error) {
+                console.error("Error inserting record", error);
+                res.status(500).json({ error: "Failed to register customer" });
+            } else {
+                console.log("Registration successful");
+                res.json({ message: "Customer registered successfully" });
+            }
+        }
+    );
+});
+
 
 module.exports = router;
